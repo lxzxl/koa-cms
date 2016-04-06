c4340dfacc56078ff170bad77538495a23eb617b
sli <sli@movoto.com>
2016-04-06 18:06:45 +0800

    add some route test cases.

--- test/routeSpec.js
+++ test/routeSpec.js
@@ -5,26 +5,41 @@ const app = require('../app');
 const request = superagent(app.listen());
 
 describe('Routes', () => {
-  describe('GET /', () => {
-    it('should return 200', done => {
-      request
-        .get('/')
-        .expect(200, done);
+    describe('GET /', () => {
+        it('should return 404', done => {
+            request
+                .get('/')
+                .expect(404, done);
+        });
     });
-  });
-  describe('GET /messages', () => {
-    it('should return 200', done => {
-      request
-        .get('/messages')
-        .expect('Content-Type', /json/)
-        .expect(200, done);
+    describe('GET /admin', () => {
+        it('should return 200', done => {
+            request
+                .get('/admin')
+                .expect(200, done);
+        });
     });
-  });
-  describe('GET /messages/notfound', () => {
-    it('should return 404', done => {
-      request
-        .get('/messages/notfound')
-        .expect(404, done);
+    describe('GET /services', () => {
+        it('should return 404', done => {
+            request
+                .get('/services')
+                .expect(404, done);
+        });
     });
-  });
+    describe('GET /services/users', () => {
+        it('should return 200', done => {
+            request
+                .get('/services/users')
+                .expect('Content-Type', /json/)
+                .expect(200, done);
+        });
+    });
+    describe('GET /services/users/0', () => {
+        it('should return 200', done => {
+            request
+                .get('/services/users')
+                .expect('Content-Type', /json/)
+                .expect(200, done);
+        });
+    });
 });
